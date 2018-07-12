e1580967da18691ac43007000c3269208b28ec05
cice <cice@Aula2macpove06.local>
2018-07-12 21:46:50 +0200

    Initial commit

---
+++ src/main/java/com/cice/GestionFacturacion/GestionFacturacion/GestionFacturacionApplication.java
@@ -0,0 +1,12 @@
+package com.cice.GestionFacturacion.GestionFacturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFacturacionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cice/GestionFacturacion/GestionFacturacion/GestionFacturacionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cice.GestionFacturacion.GestionFacturacion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestionFacturacionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
